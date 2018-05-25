9cf8e7edfd8ce76b3da1303635765064f48a1d20
Hobo Hoo <michaelhoo34@gmail.com>
2018-05-25 11:28:02 -0400

    Project Files

---
+++ Assets/obstaclespawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class obstaclespawner : MonoBehaviour {
+	public GameObject marker;
+	public GameObject rock;
+	public Text timertext;
+	Vector3 markerpos;
+	int minutes;
+	float minutestimer;
+	float timer;
+	float seconds;
+	// Use this for initialization
+	void Start () {
+		timer = Random.Range (5f, 15f);
+		minutestimer = 180f;
+		minutes = 2;
+		seconds = 60f;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		markerpos = marker.transform.position;
+		//Debug.Log (timer);
+		timer -= Time.deltaTime;
+		minutestimer -= Time.deltaTime;
+		seconds -= Time.deltaTime;
+		if (timer <= 0) {
+			Instantiate (rock, markerpos, Quaternion.identity);
+			timer = Random.Range (5f, 15f);
+		}
+		if (minutestimer <= 120f) {
+			minutes = 1;
+		}
+		if (minutestimer <= 60f) {
+			minutes = 0;
+		}
+		if (seconds <= 0) {
+			seconds = 60f;
+		}
+		timertext.text = minutes + " minutes " + Mathf.Round (seconds) + " seconds ";
+
+	}
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+	public CharacterController cc; 
+	public float speed = 4f; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W)){
+			cc.Move (transform.forward*speed*Time.deltaTime); 
+		} 
+		if (Input.GetKeyDown (KeyCode.S)) { 
+			cc.Move (transform.forward * -speed * Time.deltaTime); 
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			transform.Rotate (0f, -90f, 0f); 
+		}
+		if (Input.GetKeyDown (KeyCode.D)) { 
+			transform.Rotate (-90f, 0f, 0f); 
+}
+}
+}
