62f94ce0c7a4de6a92f5fb89ec719571fd9fdb1a
Unknown <mh4556@nyu.edu>
2018-06-15 17:00:42 -0700

    FINAL VERSION COMPLETE

--- Assets/Scripts/enemyai.cs
+++ Assets/Scripts/enemyai.cs
@@ -24,10 +24,11 @@ public class enemyai : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		distance = Vector3.Distance (player.position, transform.position);
-		Debug.Log (distance);
 		if (distance <= 5000) {
 			transform.position = Vector3.MoveTowards (transform.position, player.position, -1 * speed * Time.deltaTime);
-		}
+            transform.LookAt(player);
+
+        }
 		
 	}
 }
